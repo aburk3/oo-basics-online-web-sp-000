211803e648b2e2fa6534b954a009317d280a2fde
Austin  Burke <aburk3@gmail.com>
2018-10-05 18:22:43 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,8 @@
 class Shoe
-  attr_reader :brand, :color, :size, :material, :condition
+  attr_reader :color, :size, :material, :condition
   
-  # def initialize(brand)
-  #   @brand = brand 
-  # end
+  def initialize(brand)
+    @brand = brand 
+  end
   
 end
